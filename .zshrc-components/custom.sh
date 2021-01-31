9053a91648126633c83cae43759bc7638ebe1356
Adi Iyengar <aditya7iyengar@gmail.com>
2021-01-31 12:44:04 -0500

    Add simple custom zsh config

--- .zshrc-components/custom.sh
+++ .zshrc-components/custom.sh
@@ -2,3 +2,12 @@
 # This is a template file.
 # Put your custom zshrc config/secrets here
 #=============================================================
+
+# Path to your oh-my-zsh installation.
+export ZSH="/root/.oh-my-zsh"
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="robbyrussell"
